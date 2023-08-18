d6c1fc92fadecee97f1e3ca67539fa2f8a86384f
Jed Borseth <jedborseth@Jeds-MacBook-Pro.local>
2023-08-18 14:27:41 -0700

    emails are working on live site and look nicer

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -43,52 +43,46 @@ export const post: APIRoute = async ({ request }) => {
     try {
       const data = await resend.emails.send({
         from: `${name} <onboarding@resend.dev>`,
-        to: ["jedborseth@gmail.com"],
+        to: ["jedborseth@gmail.com", "mcborseth@gmail.com"],
         subject: `From: ${email}`,
-        html: `<!DOCTYPE html>
-      <html lang="en">
-      <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <style>
-        body {
-          font-family: Arial, sans-serif;
-          margin: 0;
-          padding: 0;
-          background-color: #f4f4f4;
-        }
-        .container {
-          max-width: 600px;
-          margin: 0 auto;
-          margin-top: 50px;
-          padding: 20px;
-          background-color: #212121;
-          border-radius: 5px;
-          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-        }
-        h1 {
-          color: #333333;
-        }
-        p {
-          color: #666666;
-        }
-        .button {
-          display: inline-block;
-          padding: 10px 20px;
-          background-color: #d2d9e5;
-          color: #ffffff;
-          text-decoration: none;
-          border-radius: 3px;
-        }
-      </style>
-      </head>
-      <body>
-        <div class="container">
-          <h1>Website: ${name} - ${email}  </h1>
-          <p>${message}</p>
-          <a href="https://rlpackaging.ca" class="button">Visit Website</a>
-        </div>
-      </body>
+        html: `<html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              margin: 0;
+              padding: 0;
+              background-color: #494949;
+              color: #ffffff;
+            }
+            .container {
+              max-width: 600px;
+              margin: 0 auto;
+              margin-top: 50px;
+              padding: 20px;
+              background-color: #212121;
+              border-radius: 5px;
+              box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            }
+            .button {
+              display: inline-block;
+              padding: 10px 20px;
+              background-color: #1d4f82;
+              color: #ffffff;
+              text-decoration: none;
+              border-radius: 3px;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>${name} - ${email}</h1>
+            <p>${message}</p>
+            <a href="mailto:${email}" class="button">Reply</a>
+          </div>
+        </body>
       </html>
       `,
       });
