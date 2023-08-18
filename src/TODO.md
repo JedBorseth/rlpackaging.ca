dd291a357616c92e218024ca88e8c321c511882c
Jed Borseth <jedborseth@Jeds-MacBook-Pro.local>
2023-08-18 14:08:43 -0700

    emails might be working on live site

--- src/TODO.md
+++ src/TODO.md
@@ -1,7 +1,6 @@
 # THINGS TO DO
 
 - Change REsend domain to come from rlpackage
-- Set Vercel domain stuff
 - Google maps api and map stuff
 - Google Business Account description and images
 - RECaptcha setup for email form
--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -1,9 +1,9 @@
 import type { APIRoute } from "astro";
 import { z } from "zod";
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
 export const post: APIRoute = async ({ request }) => {
-  const user = "rlpackaging.ca";
-  const pass = "c7WII5nGVlmOvn53";
+  const api_key = "re_GahC8JBr_Gu4WseF9od9hDZB6kuUJHyEv";
   const data = await request.formData();
   const name = data.get("name");
   const email = data.get("email");
@@ -14,7 +14,7 @@ export const post: APIRoute = async ({ request }) => {
       JSON.stringify({
         message: "Missing required fields",
       }),
-      { status: 420 }
+      { status: 400 }
     );
   }
   const schema = z.object({
@@ -36,4 +36,80 @@ export const post: APIRoute = async ({ request }) => {
       { status: 400 }
     );
   }
+
+  const resend = new Resend(api_key);
+
+  (async function () {
+    try {
+      const data = await resend.emails.send({
+        from: `${name} <onboarding@resend.dev>`,
+        to: ["jedborseth@gmail.com"],
+        subject: `From: ${email}`,
+        html: `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          margin: 0;
+          padding: 0;
+          background-color: #f4f4f4;
+        }
+        .container {
+          max-width: 600px;
+          margin: 0 auto;
+          margin-top: 50px;
+          padding: 20px;
+          background-color: #212121;
+          border-radius: 5px;
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+          color: #333333;
+        }
+        p {
+          color: #666666;
+        }
+        .button {
+          display: inline-block;
+          padding: 10px 20px;
+          background-color: #d2d9e5;
+          color: #ffffff;
+          text-decoration: none;
+          border-radius: 3px;
+        }
+      </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Website: ${name} - ${email}  </h1>
+          <p>${message}</p>
+          <a href="https://rlpackaging.ca" class="button">Visit Website</a>
+        </div>
+      </body>
+      </html>
+      `,
+      });
+
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      return new Response(
+        JSON.stringify({
+          message: `Error: ${error}}`,
+        }),
+        { status: 500 }
+      );
+    }
+  })();
+
+  console.log(`Feedback from ${name} <${email}>: ${message}`);
+  return new Response(
+    JSON.stringify({
+      message: "Success!",
+    }),
+    { status: 200 }
+  );
 };
