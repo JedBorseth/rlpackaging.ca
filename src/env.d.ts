97686adcbf0fa577c37117f8a73066d24f683f47
RLPOP <rlpop@pop-os.localdomain>
2023-10-23 15:15:38 -0700

    started shopfront and stuff

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly SQUARE_ACCESS_TOKEN: string;
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
