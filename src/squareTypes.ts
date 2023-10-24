8e75b1c3e57620da7e2aa945559720090b90c3fe
Jed Borseth <jedborseth@Jeds-MacBook-Pro.local>
2023-10-24 13:31:31 -0700

    shop functionality no styles yet

---
+++ src/squareTypes.ts
@@ -0,0 +1,48 @@
+export type SearchCatalogTypes = {
+  result: {
+    items?: [
+      {
+        itemData?: {
+          name?: string;
+          description?: string;
+          taxIds?: string[];
+          variations: any;
+          productType?: string;
+          skipModifierScreen?: boolean;
+          descriptionHtml?: string;
+          descriptionPlaintext?: string;
+          isArchived?: boolean;
+        };
+        id: string;
+        updatedAt: string;
+        version: bigint;
+        isDeleted: boolean;
+        presentAtAllLocations: boolean;
+        inventoryCount: number;
+      }
+    ];
+  };
+};
+
+export type retrieveCatalogTypes = {
+  object: {
+    type: string;
+    id: string;
+    updatedAt: string;
+    version: bigint;
+    isDeleted: boolean;
+    presentAtAllLocations: boolean;
+    itemData: {
+      name: string;
+      description: string;
+      taxIds: string[];
+      variations: any;
+      productType: string;
+      skipModifierScreen: boolean;
+      imageIds: string[];
+      descriptionHtml: string;
+      descriptionPlaintext: string;
+      isArchived: boolean;
+    };
+  };
+};
