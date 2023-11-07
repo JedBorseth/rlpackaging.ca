75d13e909d710072ca5ccc11e855b3c1d59f5b39
Jed Borseth <jedborseth@Jeds-MacBook-Pro.local>
2023-11-07 14:06:53 -0800

    updated TODO.md

--- src/TODO.md
+++ src/TODO.md
@@ -1,7 +1,11 @@
 # THINGS TO DO
 
 - Change REsend domain to come from rlpackage
-- Google maps api and map stuff
-- Google Business Account description and images
-- RECaptcha setup for email form
-- Fix styling as it looks bad
+- USA within 50km still goes through as shippable
+- cart page: change quantity of items in cart
+- cart button: quick edit / del option
+- cart button: change styles as they don't look good
+
+- Cart Stuff: add buy all functionality **\***
+
+- Cart Page styles are garbo
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -11,16 +11,31 @@ const CartComponent = () => {
   const removeItem = (id: string) => {
     cart.set(cartItems.filter((item) => item.id !== id));
   };
+  const changeQuantity = (id: string, quantity: number) => {
+    cart.set(
+      cartItems.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            quantity,
+          };
+        }
+        return item;
+      })
+    );
+  };
 
   useEffect(() => {
     cart.subscribe(() => {
-      setCartItems([...cart.get(), ...cart.get(), ...cart.get()]);
+      setCartItems(cart.get());
     });
   }, []);
   return (
     <div className="min-h-screen w-full flex justify-center items-center text-gray-900 bg-neutral-300 transition-all">
       <div>
-        <h1 className="text-3xl text-center">Your Cart</h1>
+        <h1 className="text-3xl text-center underline underline-offset-2 font-semibold">
+          Your Cart
+        </h1>
         {cartItems.length >= 0
           ? cartItems.map((item) => {
               return (
@@ -50,18 +65,18 @@ const CartComponent = () => {
                       className="flex-1 flex gap-2 justify-between"
                       onSubmit={(e) => {
                         e.preventDefault();
+                        const quantity = parseInt(
+                          e.currentTarget.quantity.value
+                        );
+                        changeQuantity(item.id, quantity);
                       }}
                     >
                       <div>
-                        <input
-                          type="number"
-                          name="quantity"
-                          id="quantity"
-                          defaultValue={item.quantity}
-                          className="w-24 text-white p-1 rounded"
-                          onChange={(e) => {}}
-                        />
-                        <button>
+                        <button
+                          onClick={() => {
+                            removeItem(item.id);
+                          }}
+                        >
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="16"
@@ -72,6 +87,13 @@ const CartComponent = () => {
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                           </svg>
                         </button>
+                        <input
+                          type="number"
+                          name="quantity"
+                          id="quantity"
+                          defaultValue={item.quantity}
+                          className="w-24 text-white p-1 rounded"
+                        />
                       </div>
 
                       <button type="submit" className="btn btn-sm btn-neutral">
@@ -84,7 +106,7 @@ const CartComponent = () => {
             })
           : null}
         {cartItems.length !== 0 ? (
-          <div className="flex justify-between w-full pt-20">
+          <div className="flex justify-between w-full p-20">
             <button
               className="btn"
               onClick={() => {
