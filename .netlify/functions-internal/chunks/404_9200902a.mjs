d08c7b3bbe02efa54b60bb545cbb482c259e39b6
Jed Borseth <95988576+JedBorseth@users.noreply.github.com>
2023-11-07 08:36:00 -0800

    Delete .netlify directory

--- .netlify/functions-internal/chunks/404_9200902a.mjs
+++
@@ -1,8 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import 'react';
-import 'react-dom/server';
-
-const page = () => import('./pages/404_02ed58ba.mjs').then(n => n._);
-
-export { page };
--- .netlify/functions-internal/chunks/index_01d324c2.mjs
+++
@@ -1,8 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import 'react';
-import 'react-dom/server';
-
-const page = () => import('./pages/index_8739780d.mjs').then(n => n.i);
-
-export { page };
