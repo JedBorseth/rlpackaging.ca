a7c9bdf51b97ce355a507aea67c59de7bb308f63
Jed Borseth <jedborseth@Jeds-MacBook-Pro.local>
2023-11-08 14:53:19 -0800

    added prefetch & sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap(), prefetch()],
   output: "server",
-  adapter: netlify(),
+  adapter: netlify()
 });
